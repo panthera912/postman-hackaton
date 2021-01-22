7602e839fee07f6be8ff92fc3139251eed9c351a
Alja Zorga Krizanic <alja.krizanic@gmail.com>
2021-01-22 19:45:33 +0100

    Comment

--- postmanHackathon/DetailViewController.swift
+++ postmanHackathon/DetailViewController.swift
@@ -14,8 +14,7 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
         print(recipe?.id)
         
         let url = URL(string: "https://spoonacular.com/recipes/turkish-delight-664089")
