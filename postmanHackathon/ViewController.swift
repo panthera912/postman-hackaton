800d7f2bd8394f0e1c913a1d9c9a4491b2a3ded7
Alja Zorga Krizanic <alja.krizanic@gmail.com>
2021-01-22 19:41:36 +0100

    Deleted comments

--- postmanHackathon/ViewController.swift
+++ postmanHackathon/ViewController.swift
@@ -14,31 +14,6 @@ var ing3 = String()
 class ViewController: UIViewController {
 
     
-    /*func getData() {
-        let urlString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=10da94bfb83e4fdfacf62713a4156fc0&ingredients=\(ing1),+\(ing2),+\(ing3)"
-        print(urlString)
-        let url = URL(string: urlString)
-        
-        print(url)
-        var a = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=10da94bfb83e4fdfacf62713a4156fc0&ingredients=apples,+flour,+sugar"
-        a = urlString
-        URLSession.shared.dataTask(with: URL(string: a)!) { (data, response, error) -> Void in
-            // Check if data was received successfully
-            if error == nil && data != nil {
-                do {
-                    // Convert to dictionary where keys are of type String, and values are of any type
-                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [[String: Any]]
-                    
-                    // Access specific key with value of type String
-                    //print(json)
-                    //let str = json["key"] as! String
-                } catch {
-                    // Something went wrong
-                }
-            }
-        }.resume()
-    }*/
-    
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     @IBOutlet weak var text3: UITextField!
